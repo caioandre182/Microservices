11aba571fdac535ff647828d65efc0c96698c7ec
caioandre182 <caioserralvo182@gmail.com>
2023-02-06 13:56:30 -0300

    :wrench: chore: Registrando o microservice no Eureka Server

--- msclients/src/main/java/com/caioandre/msclients/MsclientsApplication.java
+++ msclients/src/main/java/com/caioandre/msclients/MsclientsApplication.java
@@ -3,6 +3,7 @@ package com.caioandre.msclients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MsclientsApplication {
 
