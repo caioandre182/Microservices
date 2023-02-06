5d84f1a8e5d37393379cc5af587a9908fbded4a3
caioandre182 <caioserralvo182@gmail.com>
2023-02-06 14:11:03 -0300

    :sparkles: feat: Criando a classe Client no pacote Model

---
+++ msclients/src/main/java/com/caioandre/msclients/model/Client.java
@@ -0,0 +1,27 @@
+package com.caioandre.msclients.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String cpf;
+    @Column
+    private String name;
+    @Column
+    private Integer age;
+
+    public Client(String cpf, String name, Integer age) {
+        this.cpf = cpf;
+        this.name = name;
+        this.age = age;
+    }
+}
