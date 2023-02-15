89cbf22d71429a77670a5f87a5caab0e440cbac5
caioandre182 <caioserralvo182@gmail.com>
2023-02-15 00:08:27 -0300

    :sparkles: feat: Criando o método para listar os cartões abaixo do valor da renda informado

--- mscartoes/src/main/java/com/caioandre/mscartoes/controller/CartoesController.java
+++ mscartoes/src/main/java/com/caioandre/mscartoes/controller/CartoesController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/cartoes")
 @RequiredArgsConstructor
@@ -26,5 +28,12 @@ public class CartoesController {
         service.save(cartoes);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping(params = "income")
+    public ResponseEntity<List<Cartoes>> getCartoesIncome(@RequestParam("income") Long income){
+        List<Cartoes> list = service.getCartoesIncomeLessThanEqual(income);
+        return ResponseEntity.ok(list);
+    }
+
 
 }
